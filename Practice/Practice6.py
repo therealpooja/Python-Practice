00bb7daa9b85f1ea53de8c505bfcf75a9d12c4c4
POOJA\LENOVO <poojamktr20@gmail.com>
2022-10-09 15:48:46 +0530

    Python Program

--- Practice/Practice6.py
+++ Practice/Practice6.py
@@ -2,9 +2,10 @@ x=int(input("Enter the age of 1 person"))
 y=int(input("Enter the age of 2 person"))
 z=int(input("enter the age of 3 person "))
 if x>y and x>z:
-    print("x is the youngest one")
+    print("x is the oldest one")
 elif y>z and y>x:
     print("y is the oldest one")
 elif z>x and z>y:
     print("z is the greatest")
+
     
