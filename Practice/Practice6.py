b1a779dbfea8b756ba396aaebd70304975cc8a28
POOJA\LENOVO <poojamktr20@gmail.com>
2022-10-10 16:43:09 +0530

    Practice Python

--- Practice/Practice6.py
+++ Practice/Practice6.py
@@ -1,18 +1,18 @@
-x=int(input("Enter the age of 1 person"))
-y=int(input("Enter the age of 2 person"))
-z=int(input("enter the age of 3 person "))
-if x>y and x>z:
-    print("x is the oldest one")
-elif y>z and y>x:
-    print("y is the oldest one")
-elif z>x and z>y:
-    print("z is the oldest one")
-if x<y and x<z:
-    print("x is the youngest one")
-elif y<x and y<z:
-    print("y is the youngest")
-elif z<x and z<y:
-    print("z is the youngest")
+age1=int(input("Enter the age of 1 person"))
+age2=int(input("Enter the age of 2 person"))
+age3=int(input("enter the age of 3 person "))
+if age1>age2 and age1>age3:
+    print("age1 is the oldest one")
+elif age2>age3 and age2>age1:
+    print("age2 is the oldest one")
+elif age3>age1 and age3>age2:
+    print("age3 is the oldest one")
+if age1<age2 and age1<age3:
+    print("age1 is the age2 is youngest one")
+elif age2<age1 and age2<age3:
+    print("age2 is the age2 is youngest")
+elif age3<age1 and age3<age2:
+    print("age3 is the age2 is youngest")
 else :
     print("all are of same age")
 
